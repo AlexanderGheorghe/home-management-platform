c5aa9766c3b1f3c108231e84667c41c4ae8a2688
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2021-11-30 10:11:03 +0200

    init

---
+++ HomeManagementCore/HomeManagementCore/Controllers/TodosController.cs
@@ -0,0 +1,76 @@
+﻿using HomeManagementCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HomeManagementCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        public TodosController(TodoContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<TodosController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
+        {
+            return await _context.Todos.ToListAsync();
+        }
+
+        // GET api/<TodosController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todo>> GetTodo(int id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            return todo == null ? NotFound() : todo;
+        }
+
+        // POST api/<TodosController>
+        [HttpPost]
+        public async Task<ActionResult<Todo>> PostTodo(Todo todo)
+        {
+            _context.Todos.Add(todo);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTodo), new { id = todo.Id }, todo);
+        }
+
+        // PUT api/<TodosController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Todo>> PutTodo(int id, Todo todo)
+        {
+            if (id != todo.Id) return BadRequest();
+            _context.Entry(todo).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id)) return NotFound();
+                else throw;
+            }
+            return todo;
+        }
+
+        // DELETE api/<TodosController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo(int id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null) return NotFound();
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool TodoExists(int id)
+        {
+            return _context.Todos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ home-management-frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
