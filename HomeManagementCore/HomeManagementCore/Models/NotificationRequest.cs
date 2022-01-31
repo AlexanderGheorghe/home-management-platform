c9fc27a4efb841c6363951df285fc7a9a75fe1b7
vmircea <vmirceamr@gmail.com>
2022-01-31 23:59:52 +0200

    Call Java service endpoint from the .NET service

---
+++ HomeManagementCore/HomeManagementCore/Models/NotificationRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeManagementCore.Models;
+
+public class NotificationRequest
+{
+    public string userEmailAddress { get; set; }
+    public string message { get; set; }
+
+    public NotificationRequest(string emailAddress, string msg)
+    {
+        userEmailAddress = emailAddress;
+        message = msg;
+    }
+}
